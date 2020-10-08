a0dde871c431d203a52f99bfb7ff82f12c962cab
AndLydakis <andlydakis@gmail.com>
2020-10-08 19:34:56 +0100

    Section 5 - Lecture 62

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
+### Backend: 
 run with ```npm run configure-dev```
+
+### Frontend: 
+run with ```npm run start```
---
+++ frontend/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import {render} from 'react-dom';
+
+render(
+    <div>DragonStack from React</div>,
+    document.getElementById("root")
+);
