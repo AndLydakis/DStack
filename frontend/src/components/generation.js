4dc3b70f604c33722ca5f7e85c9287ca0a3e479f
AndLydakis <andlydakis@gmail.com>
2020-10-11 15:11:38 +0100

    Lecture 89: fetch generatio components

--- frontend/src/components/generation.js
+++ frontend/src/components/generation.js
@@ -1,15 +1,11 @@
 import React, {Component} from 'react';
+import {connect} from 'react-redux';
+import {fetchGeneration} from '../actions/generation';
+import fetchStates from '../reducers/fetchStates';
 
-const DEFAULT_GENERATION = {id: '', expiration: ''};
 const MINIMUM_DELAY = 3000;
 
 class Generation extends Component {
-
-    state = {
-        generation: {
-            DEFAULT_GENERATION
-        }
-    };
 
     timer = null;
 
@@ -22,33 +18,23 @@ class Generation extends Component {
         clearTimeout(this.timer);
     }
 
-    fetchGeneration = () => {
-        fetch('http://localhost:3000/generation')
-            .then(response => {
-                console.log('fetch response: ', response);
-                response.json().then(json => {
-                    console.log('json ', json)
-                    this.setState({generation: json.generation});
-                })
-            })
-            .catch(err => {
-                console.error('Could not fetch generation')
-            });
-    }
-
     fetchNextGeneration = () => {
-        this.fetchGeneration();
-
-        let delay = (new Date(this.state.generation.expiration)).getTime() - (new Date()).getTime();
-
+        this.props.fetchGeneration();
+        let delay = (new Date(this.props.generation.expiration)).getTime() - (new Date()).getTime();
         delay = Math.max(delay, MINIMUM_DELAY);
         this.timer = setTimeout(() => this.fetchNextGeneration(), delay);
     }
 
     render() {
-        console.log('Rendering Generation Component');
-        const {generation} = this.state;
-        const date_string = (new Date(generation.expiration)).toString()
+        console.log('Rendering Generation Component props', this.props);
+        const {generation} = this.props;
+        const date_string = (new Date(generation.expiration)).toString();
+        // if (generation.status === fetchStates.fetching) {
+        //     return <div>...</div>
+        // }
+        if (generation.status === fetchStates.error) {
+            return <div>Error Fetching Generation: {generation.status} </div>
+        }
         return <div>
             <h3>Generation {generation.id}.Expires on:</h3>
             <h4>{date_string}</h4>
@@ -56,4 +42,21 @@ class Generation extends Component {
     }
 }
 
-export default Generation;
+const mapStateToProps = (state) => {
+    const generation = state.generation;
+    return {generation: generation};
+}
+
+// const mapDispatchToProps = (dispatch) => {
+//     return {
+//         // dispatchGeneration: generation => dispatch(generationActionCreator(generation)),
+//         fetchGeneration: () => fetchGeneration(dispatch)
+//     }
+// };
+
+const componentConnector = connect(
+    mapStateToProps,
+    {fetchGeneration}
+);
+
+export default componentConnector(Generation);
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,14 +1,25 @@
 import React from 'react';
 import {render} from 'react-dom';
+import {createStore, applyMiddleware} from 'redux';
+import {Provider} from 'react-redux';
+import thunk from 'redux-thunk'
 import Generation from './components/generation'
 import Dragon from './components/dragon'
+import rootReducer from './reducers';
 import './css/index';
 
+const store = createStore(rootReducer,
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+        applyMiddleware(thunk)
+);
+
 render(
-    <div>
-        <h2>DragonStack</h2>
-        <Generation/>
-        <Dragon/>
-    </div>,
+    <Provider store={store}>
+        <div>
+            <h2>DragonStack</h2>
+            <Generation/>
+            <Dragon/>
+        </div>
+    </Provider>,
     document.getElementById("root")
 );
